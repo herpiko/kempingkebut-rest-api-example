1044892b6c59781496d217a431227b69befe161b
Akhmat Safrudin <somat@artikulpi.com>
2019-09-01 20:20:35 +0700

    Allow CORS, add readme.

--- server.js
+++ server.js
@@ -18,6 +18,15 @@ mongoose.connection.on('error',
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Allow CORS
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.get('/', (req, res) => {
  res.json({"message" : "Hello world!"});
 });
